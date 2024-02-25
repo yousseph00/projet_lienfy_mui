82a30875bed909a5f09240c7e1fe2666b4ea5cbc
yousseph00 <127420042+yousseph00@users.noreply.github.com>
2024-02-25 16:20:43 +0000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 import Button from "@mui/material/Button";
 import Input from "@mui/material/Input";
+import { Box, Container, Grid, TextField, Typography } from '@mui/material';
 
 
 export default function Home() {
@@ -37,103 +38,162 @@ export default function Home() {
         </div>
       </div>
       </section>
-
       <section>
-      <div className="bg-gray-200 flex flex-col gap-[21px] items-center justify-start p-[45px] md:px-10 sm:px-5 w-full">
-        
-            Creez un compte et commencez à raccourcir vos liens
-          <div className="bg-white-A700 border border-gray-600 border-solid flex flex-col md:gap-10 gap-[65px] items-start justify-end max-w-[1232px] mb-10 mx-auto p-[45px] md:px-5 rounded-[12px] w-full">
-            <div className="flex flex-col gap-[9px] justify-start ml-1 md:ml-[0] mt-[30px] w-full">
+        <Box bgcolor="#E5E7EB" display="flex" flexDirection="column" gap="21px" alignItems="center" justifyContent="flex-start" padding="45px" maxWidth="100%" margin="0 auto">
+          <Typography variant="h2" color="text.primary" gutterBottom style={{fontWeight: 'bold', fontSize:"4rem"}}>
+            Créez un compte et commencez à raccourcir vos liens
+          </Typography>
+          <Box bgcolor="#FFFFFF" border="2px solid #6B7280" display="flex" flexDirection="column" gap="65px" alignItems="flex-start" justifyContent="flex-end" maxWidth="1232px" marginBottom="10px" padding="45px" borderRadius="12px" width="100%">
+            <Box display="flex" flexDirection="column" gap="9px" alignItems="flex-start" justifyContent="flex-start" marginLeft="1px" marginTop="30px" width="100%">
+              <Typography variant="body1" color="text.primary" textAlign="center">
                 Entrer le lien à raccourcir
-              <Input
-                name="groupTwo"
+              </Typography>
+              <TextField
+                id="link-input"
+                variant="outlined"
                 placeholder="https : // aefrduje458gfghgjGJKfvv7azeegbk;ckbldjdtkgllgl"
-                className="font-medium p-0 placeholder:p-gray-400 p-center p-xl w-full"
-              ></Input>
-            </div>
-            <Button
-              className="bg-orange-500 cursor-pointer font-medium h-[66px] min-w-[839px] md:min-w-full ml-1 md:ml-[0] p-2xl md:p-[22px] p-center sm:p-xl"
-            >
+                fullWidth
+              />
+            </Box>
+            <Button style={{cursor: 'pointer',textAlign: 'center' , fontWeight: '500',marginTop:"50px",backgroundColor: "#FF7F2A", fontSize:"1.80rem"}} variant="contained">
               Inscrivez-vous gratuitement et commencez à raccourcir vos liens !
             </Button>
-        </div>
-      </div> 
-      </section>
-
+          </Box>
+        </Box>
+      </section> 
       <section>
-      <div className="bg-white flex flex-col items-center justify-end p-7 sm:px-5 w-full">
-        <div className="flex flex-col gap-[37px] items-center justify-start max-w-[1233px] mt-[19px] mx-auto md:px-5 w-full">
-          <div className="flex flex-col gap-[19px] items-center justify-start w-full">
-            <p>
-                C’est quoi lienfy ?
-            </p>
-            <div className="flex items-center space-x-4 justify-between">
-              <p>
-                  <>
-                    Lienfy simplifie le partage d&#39;URL en fournissant une
-                    solution de raccourcissement de liens rapide, sécurisée et
-                    conviviale. Explorez nos fonctionnalités pour comprendre
-                    comment Lienfy peut rendre votre expérience de partage en
-                    ligne plus efficace.&quot;
-                  </>
-              </p>
-              <div className="h-[175px] relative w-[28%] md:w-full">
-                <img src="images/img_lienfy.png" alt="img_lienfy" />
-              </div>
-            </div>
-          </div>
-          <div className="bg-orange-50 flex flex-col gap-[11px] items-center justify-center p-[7px] rounded-[12px] w-[49%] md:w-full">
-              <p>
-                Facilité
-              </p>
-              <img
-                className="h-12 md:h-auto object-cover w-12"
-                src="images/img_maskgroup.png"
-                alt="maskgroup"
-              />
-              <p>
-                <>
-                  Utilisez notre raccourcisseur d&#39;URL pour rendre vos liens
-                  plus faciles à partager. Rassemblez gratuitement des
-                  statistiques et des informations sur le trafic généré par vos
-                  liens et impressionnez vos cibles avec des liens mémorables et
-                  faciles à retenir.
-                </>
-              </p>
-            </div>     
-          </div>
-          <div className="flex flex gap-[19px] items-center content-center item-stretch justify-around w-full">
-            <div className="md:h-[238px] h-[241px] relative w-1/2 md:w-full">
-              <div className="absolute bg-orange-50 flex flex-col gap-[9px] h-full inset-[0] items-center justify-center m-auto p-5 rounded-[12px] w-full">
-                <p>
+      <Box bgcolor="#FFFFFF" display="flex" flexDirection="column" alignItems="center" justifyContent="flex-end" padding="28px" maxWidth="100%">
+        <Box display="flex" flexDirection="column" gap="37px" alignItems="center" justifyContent="flex-start" maxWidth="1233px" marginTop="19px" marginX="auto" paddingX="20px" width="100%">
+          <Box display="flex" flexDirection="column" gap="19px" alignItems="center" justifyContent="flex-start" width="100%">
+            <Typography variant="h4" color="text.primary" gutterBottom>
+              C’est quoi lienfy ?
+            </Typography>
+            <Box display="flex" alignItems="center" justifyContent="space-between" width="100%">
+              <Typography variant="h5" color="text.secondary" gutterBottom>
+                Lienfy simplifie le partage d&#39;URL en fournissant une solution de raccourcissement de liens rapide, sécurisée et conviviale. Explorez nos fonctionnalités pour comprendre comment Lienfy peut rendre votre expérience de partage en ligne plus efficace.
+              </Typography>
+              <Box height="175px" width="100%" maxWidth="100%">
+                <img src="images/img_lienfy.png" alt="img_lienfy" style={{ flex: '1', maxWidth: '100%', height: 'auto', objectFit: 'cover', width: '100%' }} />
+              </Box>
+            </Box>
+          </Box>
+          <Box bgcolor="#FF9800" display="flex" flexDirection="column" gap="11px" alignItems="center" justifyContent="center" padding="7px" borderRadius="12px" width="75%" maxWidth="100%">
+            <Typography variant="h6" color="text.secondary" gutterBottom>
+              Facilité
+            </Typography>
+            <img src="images/img_maskgroup.png" alt="maskgroup" style={{ height: '48px', width: '48px', objectFit: 'cover' }} />
+            <Typography variant="body1" color="text.secondary" align="center">
+              Utilisez notre raccourcisseur d&#39;URL pour rendre vos liens plus faciles à partager. Rassemblez gratuitement des statistiques et des informations sur le trafic généré par vos liens et impressionnez vos cibles avec des liens mémorables et faciles à retenir.
+            </Typography>
+          </Box>
+          <Box display="flex" gap="19px" alignItems="center" justifyContent="space-around" width="100%">
+            <Box display="flex" flexDirection="column" alignItems="center" bgcolor="#FF9800"  position="relative" height="241px" width="100%" maxWidth="100%" borderRadius="14px">
+                <Typography variant="h6" color="text.secondary" gutterBottom style={{marginTop:"15px"}}>
                   Optimisation
-                </p>
-                <img
-                  className="h-12 md:h-auto object-cover w-12"
-                  src="images/img_maskgroup_49x48.png"
-                  alt="maskgroup_One"
-                />
-              </div>
-              <p>
-                Une solution pour optimiser l&#39;information face aux limites de caractères posées par certains canaux de communication. Notre plateforme vous aide à élargir l&#39;étendu de votre message et captiver votre cible par des liens simples et attractifs
-              </p>
-            </div>
-            <div className="mt-12 bg-orange-50 flex md:flex-1 flex-col items-center justify-center p-3 rounded-[12px] w-1/2 md:w-full">
-              <p>
+                </Typography>
+                <img src="images/img_maskgroup_49x48.png" alt="maskgroup_One" style={{ height: '48px', width: '52px', objectFit: 'cover' }}/> 
+              <Typography variant="body1" color="text.secondary" align="center" style={{ position: 'absolute', bottom: '0', left: '0', right: '0', margin: 'auto', width: '92%' }}>
+                Une solution pour optimiser l&#39;information face aux limites de caractères posées par certains canaux de communication. Notre plateforme vous aide à élargir l&#39;étendue de votre message et captiver votre cible par des liens simples et attractifs
+              </Typography>
+            </Box>
+            <Box bgcolor="#FF9800" display="flex" flexDirection="column" alignItems="center" justifyContent="center" padding="3px" borderRadius="14px" width="100%" maxWidth="100%" marginTop="12px">
+              <Typography variant="h6" color="text.secondary" gutterBottom>
                 Fiabilité
-              </p>
-              <img
-                className="h-12 md:h-auto object-cover w-12"
-                src="images/img_maskgroup_52x48.png"
-                alt="maskgroup_Two"
-              />
-              <p>
+              </Typography>
+              <img src="images/img_maskgroup_52x48.png" alt="maskgroup_Two" style={{ height: '48px', width: '52px', objectFit: 'cover' }} />
+              <Typography variant="body1" color="text.secondary" align="center" style={{ marginTop: '12px', marginBottom: '20px', width: '100%' }}>
                 Instaurez et Augmentez la fiabilité de vos liens lorsque ceux-ci sont longs et/ou incluent une série de variables ambigu. Lienfy est conçue pour vous aider à renforcer la sécurité de vos privilèges en générant des liens permanents depuis une source fiable.
-              </p>
-            </div>
-          </div>
-        </div>
-        </section>  
+              </Typography>
+            </Box>
+          </Box>
+        </Box>
+      </Box>
+    </section>
+    <section>
+      <Box bgcolor="#E5E7EB" display="flex" flexDirection="column" justifyContent="space-between" alignItems="center" padding="45px" maxWidth="100%" width="100%">
+        <Box display="flex" justifyContent="center" marginTop="5px">
+          <Typography variant="h3" fontWeight="bold">Ce que disent nos visiteurs</Typography>
+        </Box>
+        <Box display="flex" flexDirection="row" justifyContent="space-around" marginTop="5px">
+          <Box display="flex" flexDirection="column" justifyContent="center" alignItems="center" bgcolor="#FFFFFF" marginTop="12px" padding="4px" borderRadius="12px">
+            <img src="images/img_ellipse1.png" alt="ellipseOne" style={{ height: '97px', width: '97px', borderRadius: '50%' }} />
+            <Typography variant="body1" fontWeight="bold" textAlign="center" marginTop="5px">Cody Fisher</Typography>
+            <Typography variant="body1" textAlign="center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore...</Typography>
+          </Box>
+          <Box display="flex" flexDirection="column" justifyContent="center" alignItems="center" bgcolor="#FFFFFF" marginTop="12px" padding="4px" borderRadius="12px">
+            <img src="images/img_ellipse1.png" alt="ellipseOne" style={{ height: '97px', width: '97px', borderRadius: '50%' }} />
+            <Typography variant="body1" fontWeight="bold" textAlign="center" marginTop="5px">Cody Fisher</Typography>
+            <Typography variant="body1" textAlign="center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore...</Typography>
+          </Box>
+          <Box display="flex" flexDirection="column" justifyContent="center" alignItems="center" bgcolor="#FFFFFF" marginTop="12px" padding="4px" borderRadius="12px">
+            <img src="images/img_ellipse1.png" alt="ellipseOne" style={{ height: '97px', width: '97px', borderRadius: '50%' }} />
+            <Typography variant="body1" fontWeight="bold" textAlign="center" marginTop="5px">Cody Fisher</Typography>
+            <Typography variant="body1" textAlign="center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore...</Typography>
+          </Box>
+        </Box>
+      </Box>
+    </section>
+    <section>
+      <Box bgcolor="#F9FAFB" display="flex" flexDirection="column" alignItems="center" justifyContent="flex-start" padding="46px" maxWidth="100%" width="100%">
+        <Typography variant="h3" fontWeight="bold" textAlign="center" className="text-4xl sm:text-32px md:text-34px text-black-900">FAQ</Typography>
+        <Box display="flex" flexDirection="column" gap="37px" alignItems="center" justifyContent="flex-start" maxWidth="1230px" marginBottom="25px" marginTop="37px" marginLeft="auto" marginRight="auto" padding="27px" width="100%">
+          <Box bgcolor="#FFFFFFA7" border="1px solid #CBD5E0" borderRadius="14px" padding="27px" width="100%">
+            <Typography variant="h4" fontWeight="bold" textAlign="center" className="leading-17px mt-0.5 md:text-3xl sm:text-28px text-32px text-black-900">
+              <span className="md:text-22px sm:text-xl text-orange-500 font-poppins text-2xl font-medium">1. Q : Pourquoi raccourcir un lien ?<br />R</span>
+              <span className="md:text-22px sm:text-xl text-gray-600 font-poppins text-2xl font-light"> </span>
+              <span className="md:text-22px sm:text-xl text-yellow-900 font-poppins text-2xl font-medium">:</span>
+              <span className="md:text-22px sm:text-xl text-gray-600 font-poppins text-2xl font-light">
+                Les liens raccourcis sont plus esthétiques et prennent moins de place. Ils sont souvent utilisés sur les plateformes de médias sociaux où l&#39;espace est limité.
+              </span>
+            </Typography>
+          </Box>
+          {/* Repeat for other FAQs */}
+          <Box bgcolor="#FFFFFFA7" border="1px solid #CBD5E0" borderRadius="14px" padding="27px" width="100%">
+            <Typography variant="h4" fontWeight="bold" textAlign="center" className="leading-17px mt-0.5 md:text-3xl sm:text-28px text-32px text-black-900">
+              <span className="md:text-22px sm:text-xl text-orange-500 font-poppins text-2xl font-medium">2. Q : Quels sont les avantages de raccourcir un lien ?<br />R</span>
+              <span className="md:text-22px sm:text-xl text-gray-600 font-poppins text-2xl font-light"> </span>
+              <span className="md:text-22px sm:text-xl text-yellow-900 font-poppins text-2xl font-medium">:</span>
+              <span className="md:text-22px sm:text-xl text-gray-600 font-poppins text-2xl font-light">
+                Amélioration de l&#39;esthétique des liens <br />
+                Économie d&#39;espace sur les supports limités en caractères <br />
+                Suivi des clics pour évaluer la popularité ou l&#39;efficacité du lien
+              </span>
+            </Typography>
+          </Box>
+          {/* Repeat for other FAQs */}
+          <Box bgcolor="#FFFFFFA7" border="1px solid #CBD5E0" borderRadius="14px" padding="27px" width="100%">
+            <Typography variant="h4" fontWeight="bold" textAlign="center" className="leading-17px mt-0.5 md:text-3xl sm:text-28px text-32px text-black-900">
+              <span className="md:text-22px sm:text-xl text-orange-500 font-poppins text-2xl font-medium">3. Q : Est-ce sécuritaire d&#39;utiliser des liens raccourcis ?<br />R</span>
+              <span className="md:text-22px sm:text-xl text-gray-600 font-poppins text-2xl font-light"> </span>
+              <span className="md:text-22px sm:text-xl text-yellow-900 font-poppins text-2xl font-medium">:</span>
+              <span className="md:text-22px sm:text-xl text-gray-600 font-poppins text-2xl font-light">
+                En général, oui. Cependant, soyez conscient que les liens raccourcis peuvent masquer la destination réelle, ce qui pourrait être exploité à des fins malveillantes. Utilisez des services réputés pour minimiser les risques.
+              </span>
+            </Typography>
+          </Box>
+        </Box>
+        <Box bgcolor="#FFFFFFA7" display="flex" flexDirection="column" alignItems="center" justifyContent="flex-start" padding="2px" width="100%" boxShadow="4px 4px 4px rgba(0, 0, 0, 0.25)">
+          <Box display="flex" alignItems="center" justifyContent="space-between" padding="4px">
+            <Box display="flex" flexDirection="column" gap="8px" alignItems="flex-start" justifyContent="flex-start" marginBottom="3px" marginTop="55px" width="568px">
+              <Box display="flex" flexDirection="column" gap="3" alignItems="flex-start" justifyContent="flex-start" width="100%">
+                <Typography variant="h4" fontWeight="semibold" className="md:text-3xl sm:text-28px text-32px text-black-900">
+                  <>
+                    Reduisez la longueur de vos liens <br />
+                    d’un seul clic !
+                  </>
+                </Typography>
+                <Typography variant="body1" className="leading-32px max-w-568px md:max-w-full text-base text-gray-600">
+                  Raccourcissez vos url pour une expérience de partage plus fluide et plus rapide
+                </Typography>
+              </Box>
+              <Button className="cursor-pointer bg-orange-500 font-medium h-66px min-w-280px text-2xl md:text-22px text-center sm:text-xl">Raccourcir votre lien</Button>
+            </Box>
+            <img src="images/img_link.png" alt="link" style={{ height: '200px', width: '200px', objectFit: 'cover' }}/>
+          </Box>
+        </Box>
+      </Box>
+    </section>
       </>
   );
 }
+
