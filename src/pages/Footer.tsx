90a1f9164aeef7a4e4f8737e8a32f1f6d17685b3
Youssou Ndiaye <yousseph@Hack.home>
2024-02-26 11:23:51 +0000

    page_d'accueil

--- src/pages/Footer.tsx
+++ src/pages/Footer.tsx
@@ -7,25 +7,29 @@ const Footer = () => {
 
 <footer style={{ backgroundColor: '#1a202c', color: '#fff', padding: '32px' }}>
   <Container maxWidth="lg">
-    <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'flex-start', justifyContent: 'space-start', gap: '16px' }}>
-      <div style={{ marginBottom: '16px', display: 'flex', alignItems: 'center',justifyContent: 'space-around', gap: '8px', flexDirection: 'column' }}>
-        <img src="/images/img_logopng.png" alt="logopng" style={{ height: '32px' }} />
-        <p>
-          Le raccourcisseur durl panafricain.
-        </p>
-        <p style={{marginTop: '40px'}}>
+    <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'flex-start', justifyContent: 'space-around', gap: '16px' }}>
+      <div style={{ marginBottom: '16px', display: 'flex', alignItems: 'start',justifyContent: 'space-start', gap: '8px', flexDirection: 'column' }}>
+        <div><
+            img src="/images/img_logopng.png" alt="logopng" style={{ height: '32px' }} />
+        </div>
+            <div>
+            Le raccourcisseur durl panafricain.
+            </div>
+        <div style={{marginTop: '40px'}}>
           Sicap Mbao <br />
           Boutique Baye Niass <br />
           Dakar, Senegal <br />
+        <div style={{marginTop: '40px'}}> 
           <span style={{ fontWeight: 'bold' }}>Phone:</span> +221 77 860 60 64 <br />
           <span style={{ fontWeight: 'bold' }}>Email:</span> xaralatech@gmail.com
-        </p>
+        </div>   
+        </div>
       </div>
       <div style={{ marginBottom: '16px' }}>
         <p style={{ fontWeight: 'bold' }}>
           Lienfy
         </p>
-        <ul style={{ listStyleType: 'none', padding: '0', margin: '0', marginTop: '8px' }}>
+        <ul style={{display:"flex", flexDirection:"column",justifyContent:"space-between" , listStyleType: 'none', padding: '0', margin: '0', marginTop: '19px' }}>
           <li>
             <Link href="/" passHref>
               <p style={{ color: '#99a3b2' }}>
@@ -77,7 +81,7 @@ const Footer = () => {
   </Container>
   <div style={{ marginTop: '40px', display: 'flex', justifyContent: 'center', color: '#99a3b2' }}>
     <p>
-      © Copyright Lienfy. All Rights Reserved. Crée par Xarala
+      © Copyright <span style={{color:"#FFFFFF",fontWeight:"bold"}}>Lienfy.</span>  All Rights Reserved. Crée par <span style={{color:"#FF7F2A",fontWeight:"bold"}}>Xarala</span>
     </p>
   </div>
 </footer>
